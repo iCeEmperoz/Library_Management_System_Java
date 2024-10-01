3ce31cd199bcea16c4b5b05013f1e5c2664aa6c3
kiennkt05 <kiennkt05@gmail.com>
2024-10-01 15:40:10 +0700

    Create Book.java

---
+++ src/main/java/LMS/Book.java
@@ -0,0 +1,15 @@
+package LMS;
+
+import java.io.*;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+
+public class Book {
+    private int bookID;
+    private String title;
+    private String subject;
+    private String author;
+    private boolean isIssued;
+    
+
+}

a5f2d879e3b7da26dc0c8cf02fba267a287abbaf
kiennkt05 <kiennkt05@gmail.com>
2024-10-02 23:12:06 +0700

    Add initial GUI

--- src/main/java/LMS/Main.java
+++ src/main/java/LMS/Main.java
@@ -1,5 +1,24 @@
 package LMS;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("RegistrationForm.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/LMS/RegistrationForm.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/LMS/MainFXML.java
@@ -0,0 +1,76 @@
+package LMS;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Window;
+
+public class MainFXML {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private Label createAccountLabel;
+
+    @FXML
+    private Label forgotPasswordLabel;
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    public void initialize() {
+        submitButton.setOnAction(event -> handleSubmit());
+        createAccountLabel.setOnMouseClicked(event -> handleCreateAccount());
+
+        forgotPasswordLabel.setOnMouseClicked(event -> handleForgotPassword());
+    }
+
+    @FXML
+    private void handleSubmit() {
+        if (nameField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your name");
+            return;
+        }
+        if (emailField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your email id");
+            return;
+        }
+        if (passwordField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter a password");
+            return;
+        }
+
+        showAlert(Alert.AlertType.CONFIRMATION, gridPane.getScene().getWindow(), "Registration Successful!", "Welcome " + nameField.getText());
+    }
+
+    @FXML
+    private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+
+    @FXML
+    private void handleCreateAccount() {
+        showAlert(Alert.AlertType.CONFIRMATION, gridPane.getScene().getWindow(), "Create new Account", "Function is not available! Try later.");
+    }
+
+    @FXML
+    private void handleForgotPassword() {
+        showAlert(Alert.AlertType.CONFIRMATION, gridPane.getScene().getWindow(), "Forgot Password", "Function is not available! Try later.");
+    }
+}

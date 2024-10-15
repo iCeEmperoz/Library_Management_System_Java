8fd1858cd070edd5c9e2b9342c8e52aa74874eaa
kiennkt05 <kiennkt05@gmail.com>
2024-10-15 12:09:31 +0700

    Kien's GUI

--- src/main/java/LMS/FXML_Admin_Dashboard.java
+++ src/main/java/LMS/FXML_Admin_Dashboard.java
@@ -1,5 +1,13 @@
 package LMS;
 
-public class FXML_Admin_Dashboard {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class FXML_Admin_Dashboard extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
